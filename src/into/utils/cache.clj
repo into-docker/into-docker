10ec7ae8c6982451449ec44b79a22c1b51ef4d04
Yannick Scherer <yannick.scherer@futurice.com>
2020-04-23 20:56:27 +0200

    Make caching more robust

--- src/into/utils/cache.clj
+++ src/into/utils/cache.clj
@@ -19,13 +19,22 @@
          (map #(format "%02x" %))
          (apply str))))
 
+(defn- ->prepare-command
+  [cache-directory path]
+  (let [cache-path (.getPath (io/file cache-directory (sha1 path)))]
+    (format
+      (str "_CPTH_='%s'; _PTH_='%s';"
+           "[ -e \"$_PTH_\" ] && ("
+           "mv \"$_PTH_\" \"$_CPTH_\" && "
+           "echo \"$_PTH_\";"
+           ")")
+      cache-path
+      path)))
+
 (defn prepare-cache-command
   [cache-directory paths]
   (->> paths
-       (map #(format "mv -f '%s' '%s/%s'"
-                     %
-                     cache-directory
-                     (sha1 %)))
+       (map #(->prepare-command cache-directory %))
        (string/join "; ")
        (format "mkdir -p '%s'; %s" cache-directory)
        (format "%s; true")
@@ -38,8 +47,9 @@
       (str "_CPTH_='%s'; _PTH_='%s';"
            "[ -e \"$_CPTH_\" ] && ("
            "rm -rf \"$_PTH_\";"
-           "mv \"$_CPTH_\" \"$_PTH_\";"
+           "mv \"$_CPTH_\" \"$_PTH_\" && "
            "echo \"$_PTH_\";"
+           "rm -rf \"$_CPTH_\""
            ")")
       cache-path
       path)))
