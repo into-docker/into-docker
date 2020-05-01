e6295944db54bec238b7205a43b3f604162ea620
Yannick Scherer <yannick.scherer@futurice.com>
2020-05-01 12:08:58 +0200

    Fail if non-default build profile is empty

--- src/into/build/read_build_profile.clj
+++ src/into/build/read_build_profile.clj
@@ -1,8 +1,8 @@
 (ns into.build.read-build-profile
   (:require [into.docker :as docker]
             [into.utils
+             [log :as log]
              [data :as data]]
-            [clojure.tools.logging :as log]
             [clojure.string :as string]
             [clojure.java.io :as io]))
 
@@ -21,18 +21,28 @@
                (remove #(string/starts-with? % "#"))
                (remove string/blank?))))))
 
-(defn- log-build-profile
+(defn- assoc-build-profile
   [data env]
-  (->> env
-       (map #(str "[into]   " %))
-       (string/join "\n")
-       (log/debugf "[into] Build profile [%s]:%n%s"
-                   (get-in data [:spec :profile])))
-  env)
+  (if-let [profile (get-in data [:spec :profile])]
+    (cond (seq env)
+          (do
+            (log/debug data "Build profile [%s]:" profile)
+            (doseq [e env]
+              (log/debug data "  %s" e))
+            (assoc-in data [:instances :builder :image :env] env))
+
+          (= profile "default")
+          (log/debug data "Build profile [%s] is empty." profile)
+
+          :else
+          (assoc data
+                 :error
+                 (IllegalStateException.
+                   (format "Build profile [%s] is empty." profile))))
+    data))
 
 (defn run
   [data]
   (or (some->> (read-profile-env! data)
-               (log-build-profile data)
-               (assoc-in data [:instances :builder :image :env]))
+               (assoc-build-profile data))
       data))
