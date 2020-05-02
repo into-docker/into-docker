9675795cce81d8750e467c3738ccd662556a517b
Yannick Scherer <yannick.scherer@futurice.com>
2020-05-02 09:52:31 +0200

    Extend 'task' creation

--- src/into/utils/task.clj
+++ src/into/utils/task.clj
@@ -2,8 +2,45 @@
   (:require [into.docker.client :as client]
             [peripheral.core :as p]
             [jansi-clj.core :as jansi]
+            [clojure.string :as string]
             [clojure.tools.logging :as log]
-            [clojure.tools.cli :as cli]))
+            [clojure.tools.cli :as cli])
+  (:import [org.slf4j LoggerFactory]
+           [ch.qos.logback.classic Level Logger]))
+
+;; ## Default Options
+
+(def ^:private cli-options
+  [["-v" "--verbose" "Increase verbosity (can be used multiple times)"
+    :id :verbosity
+    :default 0
+    :update-fn inc]
+   ["-h" "--help" "Show help"]])
+
+;; ## Verbosity
+
+(defn- root-logger
+  ^Logger []
+  (LoggerFactory/getLogger "root"))
+
+(defn- set-log-level!
+  [^String value]
+  (let [^Level level (-> value
+                         (.toUpperCase)
+                         (Level/valueOf))]
+    (.setLevel (root-logger) level)))
+
+(defn- set-verbosity!
+  [{:keys [^long verbosity]}]
+  (cond (= 0 verbosity) nil
+        (= 1 verbosity) (set-log-level! "DEBUG")
+        :else (set-log-level! "TRACE")))
+
+(defmacro ^:private with-verbosity
+  [opts & body]
+  `(let [opts# ~opts]
+     (set-verbosity! (:options opts#))
+     ~@body))
 
 ;; ## Helpers
 
@@ -21,16 +58,35 @@
   :error)
 
 (defn- print-errors
-  [{:keys [errors arguments] :as opts}]
+  [_ {:keys [errors arguments] :as opts}]
   (when (seq errors)
     (doseq [error errors]
       (show-error error))
     :error))
 
 (defn- print-help
-  [usage {{:keys [help]} :options, :as opts}]
-  (when help
+  [{:keys [usage no-args?]}
+   {{:keys [help]} :options, args :arguments, :as opts}]
+  (when (or help
+            (not (or no-args? (seq args))))
     (show-help usage opts)))
+
+(letfn [(find-option [cli k]
+          (first
+           (for [[_ _ _ & {:as rst} :as opt] cli
+                 :when (= (:id rst) k)]
+             opt)))]
+  (defn- print-missing
+    [{:keys [cli needs]} {:keys [options]}]
+    (when-let [missing (seq (remove options needs))]
+      (doseq [k missing
+              :let [[short-opt long-opt] (find-option cli k)]]
+        (show-error
+         (str "Missing option: "
+              (->> [short-opt long-opt]
+                   (remove nil?)
+                   (string/join "/")))))
+      :missing)))
 
 ;; ## Subtask Wrapper
 
@@ -40,16 +96,19 @@
       "unix:///var/run/docker.sock"))
 
 (defn make
-  [{:keys [cli usage run docker?]}]
-  (fn [args]
-    (let [opts (cli/parse-opts args cli :in-order true)
-          opts     (merge
-                    {:show-help #(show-help usage opts)
-                     :show-error show-error}
-                    opts)]
-      (or (print-help usage opts)
-          (print-errors opts)
-          (if docker?
-            (p/with-start [client (client/make {:uri (get-docker-uri)})]
-              (run (assoc opts :client client)))
-            (run opts))))))
+  [{:keys [cli no-args? needs usage run docker?] :as task}]
+  (let [cli (concat cli cli-options)]
+    (fn [args]
+      (let [opts (cli/parse-opts args cli :in-order true)
+            opts     (merge
+                      {:show-help #(show-help usage opts)
+                       :show-error show-error}
+                      opts)]
+        (or (print-help task opts)
+            (print-errors task opts)
+            (print-missing task opts)
+            (with-verbosity opts
+              (if docker?
+                (p/with-start [client (client/make {:uri (get-docker-uri)})]
+                  (run (assoc opts :client client)))
+                (run opts))))))))
