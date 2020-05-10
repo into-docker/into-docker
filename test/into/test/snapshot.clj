8c1e54d13610aabe3c4834e4f15620a6e66666c1
Yannick Scherer <yannick.scherer@futurice.com>
2020-05-10 18:42:00 +0200

    Add experimental snapshot testing utility

---
+++ test/into/test/snapshot.clj
@@ -0,0 +1,33 @@
+(ns into.test.snapshot
+  (:require [clojure.test :refer [is]]
+            [clojure.java.io :as io]
+            [clojure.edn :as edn])
+  (:import [java.io PushbackReader]))
+
+(defn snapshot*
+  [path value]
+  (let [file (io/file path)]
+    (if (.isFile file)
+      (with-open [in (PushbackReader. (io/reader file))]
+        (edn/read in))
+      (do
+        (io/make-parents file)
+        (spit file (pr-str value))
+        value))))
+
+(defn create-snapshot-path
+  [file snapshot-name]
+  (let [f (io/file file)]
+    (io/file "test"
+             (.getParentFile f)
+             "snapshots"
+             (str (name snapshot-name) ".snap"))))
+
+(defmacro is-snap?
+  "Compare value with a snapshot of the given name, calling `clojure.test/is`
+   under the hood."
+  [snapshot-name value]
+  `(let [~'value ~value
+         ~'path  (create-snapshot-path ~*file* ~snapshot-name)
+         ~'snap  (snapshot* ~'path ~'value)]
+     (is ~'(= snap value))))
