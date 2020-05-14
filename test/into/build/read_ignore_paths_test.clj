02f6481c8ffbe9b9fc2ff043595da7a650192088
Yannick Scherer <yannick.scherer@futurice.com>
2020-05-14 19:12:15 +0200

    Test ignore behaviour for cache file

--- test/into/build/read_ignore_paths_test.clj
+++ test/into/build/read_ignore_paths_test.clj
@@ -4,8 +4,10 @@
              [properties :as prop]]
             [com.gfredericks.test.chuck :refer [times]]
             [clojure.spec.alpha :as s]
+            [clojure.java.io :as io]
             [into.constants :as constants]
             [into.docker.mock :as docker]
+            [into.test.files :refer [with-temp-dir]]
             [into.test.generators :refer [gen-file-with-comments]]
             [into.build.spec :as spec]
             [into.build.read-ignore-paths :as step]))
@@ -39,3 +41,18 @@
             :spec              spec}
            (step/run)
            (:ignore-paths)))))
+
+(defspec t-read-ignore-paths-should-add-cache-file-if-in-source-path (times 5)
+  (prop/for-all
+    [spec       (s/gen ::spec/spec)
+     cache-from (s/gen ::spec/cache-from)]
+    (with-temp-dir [source-path [cache-from]]
+      (let [cache-path (.getCanonicalPath (io/file source-path cache-from))
+            source-path (.getCanonicalPath source-path)]
+        (= (concat constants/default-ignore-paths [cache-from])
+           (-> {:builder-container (docker/container)
+                :spec              (assoc spec
+                                          :source-path source-path
+                                          :cache-from cache-path)}
+               (step/run)
+               (:ignore-paths)))))))
