d08f6a644ec28ec2e7f5311833d3563a293422d6
Yannick Scherer <yannick.scherer@futurice.com>
2022-03-06 12:36:28 +0100

    Version 1.1.5-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject into "1.1.4"
+(defproject into "1.1.5-SNAPSHOT"
   :description "Never write another Dockerfile."
   :url "https://github.com/into-docker/into-docker"
   :license {:name "MIT"
