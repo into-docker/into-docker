d7f87b62df819c23cbdc3e49fc3accf3590589df
Yannick Scherer <yannick.scherer@futurice.com>
2023-10-18 12:45:24 +0200

    Revert "Version 1.1.8-SNAPSHOT"
    This reverts commit ec9564cd6d1889ee5c5b553331e786a347475ca5.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject into "1.1.8-SNAPSHOT"
+(defproject into "1.1.7"
   :description "Never write another Dockerfile."
   :url "https://github.com/into-docker/into-docker"
   :license {:name "MIT"
